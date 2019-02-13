758c90b65e6211da33c087a6e41901a6251c10de
Ahnsik <ahnsik.choi@gmail.com>
2019-02-14 02:52:12 +0900

    chord table 각 액티비티 작성 중. C,C7 코드 추가

--- app/src/main/java/com/example/ahnsik/mytuner/ChordTableActivity.java
+++ app/src/main/java/com/example/ahnsik/mytuner/ChordTableActivity.java
@@ -1,5 +1,7 @@
 package com.example.ahnsik.mytuner;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +23,29 @@ public class ChordTableActivity extends AppCompatActivity {
         });
 
 
+        Button btnChord_C = (Button)findViewById(R.id.btnChord_C);
+        btnChord_C.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoActivity("ChordC_Activity");
+            }
+        });
 
+        Button btnChord_C7 = (Button)findViewById(R.id.btnChord_C7);
+        btnChord_C7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoActivity("ChordC7_Activity");
+            }
+        });
+
+    }
+
+    protected void gotoActivity(String activityName)
+    {
+        Intent i = new Intent();
+        ComponentName name= new ComponentName("com.example.ahnsik.mytuner", "com.example.ahnsik.mytuner."+activityName);
+        i.setComponent(name);
+        startActivity(i);
     }
 }
